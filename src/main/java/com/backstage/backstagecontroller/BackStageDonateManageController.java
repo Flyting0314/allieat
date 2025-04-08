668a72378d585f71bac2ff7a9aa4e4192ee05bfa
S101066509 <an79322@gmail.com>
2025-04-08 16:33:13 +0800

    加入後台捐款明細頁面與控制器，修改後端頁面部分欄位。

--- src/main/java/com/backstage/backstagecontroller/BackStageDonateManageController.java
+++ src/main/java/com/backstage/backstagecontroller/BackStageDonateManageController.java
@@ -1,12 +1,15 @@
 package com.backstage.backstagecontroller;
 
 import com.backstage.backstagedto.DonateInitResponse;
+import com.backstage.backstagedto.DonationDetailsDTO;
 import com.backstage.backstageservice.BackStageDonateManageServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/backStage")
@@ -17,6 +20,11 @@ public class BackStageDonateManageController {
     @GetMapping("/donaManage")
     public  ResponseEntity<DonateInitResponse> getInitData() {
         return backStageDonateManageServiceImpl.getInitData();
+    }
+
+    @GetMapping("/donaRecord")
+    public  ResponseEntity<DonationDetailsDTO> getDonateRecord(@RequestParam("id") Integer id) {
+        return backStageDonateManageServiceImpl.getDonateRecord(id);
     }
 
 }
