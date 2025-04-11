7a039159a6f84ed0bb97426dca626ab9550f65e8
TessHuang <tesshuang666@gmail.com>
2025-04-12 02:45:19 +0800

    1. 新增點數後台管理頁面 2. 修改會員後台管理頁面 3. 修改後台頁面的側邊欄樣式（新增店家點數核銷選項/ 點擊主選項後展開的子選項 css 有調整），增加 style_backstage.css 的樣式以作用在新的側邊欄上（現在可以在會員和點數管理頁面看到新的側邊欄樣式） 4. application.java 增加排程器的 annotation，讓系統可以使用排程功能

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -2,8 +2,10 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling  //使用排程器一定要在application加這個
 public class Application {
 
 	public static void main(String[] args) {
