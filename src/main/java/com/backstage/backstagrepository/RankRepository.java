6ab7f764cd9dc606430d77a6c18f1716ff2bb640
YL.H <y94718874hyl@gmail.com>
2025-04-14 03:01:07 +0800

    新增store、member註冊登入相關css;新增捐款排行相關;增加排行方法DonorRepository、RankRepository;新增捐款排行下拉選單

--- src/main/java/com/backstage/backstagrepository/RankRepository.java
+++ src/main/java/com/backstage/backstagrepository/RankRepository.java
@@ -2,10 +2,15 @@ package com.backstage.backstagrepository;
 
 
 
-import com.entity.RankVO;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.entity.RankVO;
+
 @Repository
 public interface RankRepository extends JpaRepository<RankVO, Integer> {
+	 List<RankVO> findAllByOrderByTotalDonationDesc();
+	 
 }
