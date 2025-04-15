e7fad4a7b388c4512cddf999ff09efea1e234ef8
S101066509 <an79322@gmail.com>
2025-04-15 16:07:27 +0800

    後台登入分層修改。

--- src/main/java/com/backstage/backstageservice/BackStageLoginServiceImpl.java
+++ src/main/java/com/backstage/backstageservice/BackStageLoginServiceImpl.java
@@ -1,6 +1,5 @@
 package com.backstage.backstageservice;
 
-
 import com.backstage.backstagedto.AdminDTO;
 import com.backstage.backstagrepository.BackStageLoginDao;
 
@@ -15,51 +14,46 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Service
 public class BackStageLoginServiceImpl implements BackStageLoginService {
-    @Autowired
-    private BackStageLoginDao backStageLogin;
-    @Autowired
-    private JwtUtil jwtUtil;
-    @Autowired
-    private PasswordEncoder encoder;
+	@Autowired
+	private BackStageLoginDao backStageLogin;
+	@Autowired
+	private JwtUtil jwtUtil;
+	@Autowired
+	private PasswordEncoder encoder;
 
-    @Override
-    public ResponseEntity<Map<String, Object>> findByAccount(AdminDTO admin) {
-        //資料Mapping
-        AdminVO adminVO =new AdminVO();
-        BeanUtils.copyProperties(admin, adminVO);
-        try {
-            List<AdminVO> adminList = backStageLogin.findByAccount(adminVO);
-            if (!adminList.isEmpty()) {
-                AdminVO foundAdmin = adminList.get(0);  // 取出結果
-                if (foundAdmin.getPassword() != null && encoder.matches(admin.getPassword(), foundAdmin.getPassword())) {
-                    //產生Token
-                    String token = jwtUtil.generateToken(foundAdmin.getAccount());
-                    //回傳值設定
-                    Map<String,Object> result = createResult("login ok");
-                    result.put("token", token);
-                    return ResponseEntity.status(HttpStatus.OK).body(result);  // 密碼正確
-                } else {
-                    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(createResult("login failed password is incorrect"));  // 密碼錯誤
-                }
-            } else {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(createResult("account does not exist"));  // 帳號不存在
-            }
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(createResult("服務異常請重試"));
-        }
-    }
+	@Override
+	public Map<String, Object> findByAccount(AdminDTO admin) {
+		// 資料Mapping
+		AdminVO adminVO = new AdminVO();
+		BeanUtils.copyProperties(admin, adminVO);
+		List<AdminVO> adminList = backStageLogin.findByAccount(adminVO);
+		if (!adminList.isEmpty()) {
+			AdminVO foundAdmin = adminList.get(0); // 取出結果
+			if (foundAdmin.getPassword() != null && encoder.matches(admin.getPassword(), foundAdmin.getPassword())) {
+				// 產生Token
+				String token = jwtUtil.generateToken(foundAdmin.getAccount());
+				// 回傳值設定
+				Map<String, Object> result = createResult("login ok");
+				result.put("token", token);
+				return result; // 密碼正確
+			} else {
+				return createResult("login failed password is incorrect"); // 密碼錯誤
+			}
+		} else {
+			return createResult("account does not exist"); 
+		}
+	}
 
-    //用於回傳狀態創建
-    private Map<String, Object> createResult(String message) {
-        Map<String, Object> result = new HashMap<>();
-        result.put("loginState", message);
-        return result;
-    }
+	// 用於回傳狀態創建
+	private Map<String, Object> createResult(String message) {
+		Map<String, Object> result = new HashMap<>();
+		result.put("loginState", message);
+		return result;
+	}
 }
