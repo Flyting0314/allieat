e2e05296bb21043685ba50c630d8943ce0ed92a8
Timothy Lu <timothy801021@gmail.com>
2025-04-16 12:14:49 +0800

    測試firebase

--- src/main/java/com/storeOrder/controller/StoreOrderController.java
+++ src/main/java/com/storeOrder/controller/StoreOrderController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("registerAndLogin/storeInfo/{storeId}/orders")
 public class StoreOrderController {
-
+	
 
 }
