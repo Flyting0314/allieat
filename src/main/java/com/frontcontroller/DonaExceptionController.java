d8e868cbfce379da6fbf2b5fb5c320c00a8344b9
YL.H <y94718874hyl@gmail.com>
2025-04-19 06:05:21 +0800

    錯誤頁更新、application切換開發版/上線版

--- src/main/java/com/frontcontroller/DonaExceptionController.java
+++ src/main/java/com/frontcontroller/DonaExceptionController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -30,4 +31,5 @@ public class DonaExceptionController {
 
         return mav;
     }
+    
 }
